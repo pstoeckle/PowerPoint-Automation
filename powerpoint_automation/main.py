c9e2d1a835b22a629bb8f08ad20ea1d4be3e99a3
Patrick Stoeckle <patrick.stoeckle@tum.de>
2021-04-07 15:13:20 +0200

    :truck: Split code

--- powerpoint_automation/main.py
+++ powerpoint_automation/main.py
@@ -1,26 +1,25 @@
 """
 Convert.
 """
-from hashlib import sha3_256
-from json import dump, load
 from logging import INFO, basicConfig, getLogger
 from os.path import isfile
 from pathlib import Path as pathlib_Path
-from subprocess import call
 from sys import platform, stdout
-from typing import Any, List, MutableMapping, Optional
+from typing import Any, List, Optional
 
 from click import Context, Path, echo, group, option
 from pptx import Presentation
-from pptx.shapes.picture import Picture
 
 from powerpoint_automation import __version__
 from powerpoint_automation.logic.add_git_info import add_git_info_internal
+from powerpoint_automation.logic.convert_presentations import (
+    convert_presentations_internal,
+)
+from powerpoint_automation.logic.remove_picture import remove_picture_internal
 
 LINUX_LIBREOFFICE = "/usr/bin/libreoffice"
 MAC_OS_SOFFICE = "/Applications/LibreOffice.app/Contents/MacOS/soffice"
 LIBRE_OFFICE = "NOT_SET"
-CACHE_FILE = ".powerpoint-automation.json"
 
 _LOGGER = getLogger(__name__)
 basicConfig(
@@ -104,19 +103,9 @@ def convert_presentations(
     """
     input_directory_path = pathlib_Path(input_directory)
     output_directory_path = pathlib_Path(output_directory)
-    _setup_output_directory(output_directory_path)
-    cache_file = input_directory_path.joinpath(CACHE_FILE)
-    content = _load_cache(cache_file)
-    files = list(input_directory_path.iterdir())
-    files = [
-        f
-        for f in files
-        if f.is_file() and f.suffix.casefold() == ".pptx" and not f.stem.startswith("~")
-    ]
-    for file in files:
-        _convert_file(content, libre_office, file, output_directory_path)
-    with cache_file.open("w") as f_write:
-        dump(content, f_write, indent=4)
+    convert_presentations_internal(
+        input_directory_path, libre_office, output_directory_path
+    )
 
 
 @option("--old-year", "-O", type=int, default=2020)
@@ -124,6 +113,9 @@ def convert_presentations(
 @_INPUT_DIRECTORY_OPTION
 @main_group.command()
 def replace_date(input_directory: str, old_year: int, new_year: int) -> None:
+    """
+    Replace a date in the slides, e.g., 2020 -> 2021.
+    """
     input_directory_path = pathlib_Path(input_directory)
     pptx_files = [
         f
@@ -175,91 +167,7 @@ def remove_picture(
         return
     hashes_set = frozenset(h.casefold() for h in hash_value)
     input_directory_path = pathlib_Path(input_directory)
-    pptx_files = [
-        f
-        for f in input_directory_path.iterdir()
-        if f.is_file() and f.suffix.casefold() == ".pptx" and not f.stem.startswith("~")
-    ]
-    for input_file in pptx_files:
-        pres = Presentation(input_file)
-        rewrite_file = False
-        for slide in pres.slides:
-            shapes_to_delete = []
-            for shape in slide.shapes:
-                if isinstance(shape, Picture):
-                    if shape.image.sha1.casefold() in hashes_set:
-                        shapes_to_delete.append(shape)
-            if len(shapes_to_delete) > 0:
-                rewrite_file = True
-                for shape_to_delete in shapes_to_delete:
-                    old_pic = shape_to_delete._element
-                    old_pic.getparent().remove(old_pic)
-        if rewrite_file:
-            if inplace:
-                _LOGGER.info(f"Rewrite file {input_file}")
-                pres.save(input_file)
-            else:
-                new_file_name = (
-                    str(input_file)
-                    .replace(".pptx", ".out.pptx")
-                    .replace(".PPTX", ".out.pptx")
-                )
-                _LOGGER.info(f"Write file {new_file_name}")
-                pres.save(new_file_name)
-
-
-def _convert_file(
-    cache_content: MutableMapping[str, str],
-    libre_office: str,
-    file: pathlib_Path,
-    output_directory_path: pathlib_Path,
-) -> None:
-    file_hash = hash_file(file)
-    if str(file) in cache_content.keys() and cache_content[str(file)] == file_hash:
-        _LOGGER.info(f"The file {file} has not changed since the last conversion.")
-        return
-    _LOGGER.info(f"Convert {file} to PDF")
-    call(
-        [
-            libre_office,
-            "--headless",
-            "--convert-to",
-            "pdf",
-            file,
-            "--print-to-file",
-            "--outdir",
-            str(output_directory_path),
-        ]
-    )
-    cache_content[str(file)] = file_hash
-
-
-def _load_cache(cache_file: pathlib_Path) -> MutableMapping[str, str]:
-    content: MutableMapping[str, str]
-    if cache_file.is_file():
-        with cache_file.open() as f_read:
-            content = load(f_read)
-    else:
-        content = {}
-    return content
-
-
-def _setup_output_directory(output_directory_path: pathlib_Path) -> None:
-    if not output_directory_path.is_dir():
-        output_directory_path.mkdir()
-
-
-def hash_file(file: pathlib_Path) -> str:
-    """
-    Tp.
-    """
-    if not file.is_file():
-        return ""
-    current_sha = sha3_256()
-    with file.open("rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            current_sha.update(chunk)
-    return current_sha.hexdigest()
+    remove_picture_internal(hashes_set, inplace, input_directory_path)
 
 
 if __name__ == "__main__":
