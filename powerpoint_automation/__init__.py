ac01ea9c9fca3025c43bda711299bf773ec880e3
Patrick Stöckle <patrick.stoeckle@siemens.com>
2024-01-30 11:30:15 +0100

    :fire: Replace deprecated code

--- powerpoint_automation/__init__.py
+++ powerpoint_automation/__init__.py
@@ -4,16 +4,10 @@
 """
 Main module.
 """
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "powerpoint-automation"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+__version__ = version("powerpoint-automation")
+
 __author__ = "Patrick Stöckle"
 __copyright__ = "Patrick Stöckle"
 __license__ = "mit"
